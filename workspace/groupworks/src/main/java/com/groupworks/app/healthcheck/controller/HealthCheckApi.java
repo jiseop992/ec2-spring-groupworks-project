42409d68521faf1e9d4a6728c5e1a75cad83f3f6
jiseop <jiseop992@gmail.com>
2024-09-13 20:08:44 +0900

    헬스케어 api 추가

---
+++ workspace/groupworks/src/main/java/com/groupworks/app/healthcheck/controller/HealthCheckApi.java
@@ -0,0 +1,20 @@
+package com.groupworks.app.healthcheck.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@CrossOrigin("*")
+public class HealthCheckApi {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> heathCheck() {
+        return ResponseEntity.ok("Success Health Check");
+    }
+}
