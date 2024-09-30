2f3bc688f5cb21061970cec9cd6eac066687b736
jiseop <jiseop992@gmail.com>
2024-09-30 13:38:21 +0900

    쿼리문 수정

--- workspace/groupworks/src/main/java/com/groupworks/app/message/dao/MessageDao.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/message/dao/MessageDao.java
@@ -8,27 +8,22 @@ import com.groupworks.app.message.vo.MessageVo;
 @Repository
 public class MessageDao {
 
-	//보낸 쪽지 조회
 	public List<MessageVo> sendList(SqlSessionTemplate sst, String memberNo) {
 		return sst.selectList("MessageMapper.sendList", memberNo);
 	}
 	
-	//받은 쪽지 조회
 	public List<MessageVo> recieveList(SqlSessionTemplate sst, String memberNo) {
 		return sst.selectList("MessageMapper.recieveList", memberNo);
 	}
 	
-	//쪽지 보내기
 	public int write(SqlSessionTemplate sst, MessageVo vo) {
 		return sst.insert("MessageMapper.insert", vo);
 	}
 	
-	//쪽지 답장
 	public int reWrite(SqlSessionTemplate sst, MessageVo vo) {
 		return sst.insert("MessageMapper.reInsert", vo);
 	}
 	
-	//쪽지 삭제
 	public int delete(SqlSessionTemplate sst, String no) {
 		return sst.update("MessageMapper.delete", no);
 	}
--- workspace/groupworks/src/main/java/com/groupworks/app/payform/service/PayFormService.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/payform/service/PayFormService.java
@@ -17,37 +17,30 @@ public class PayFormService {
 	private final PayFormDao dao;
 	private final SqlSessionTemplate sst;
 	
-	//지출결의서 전체조회
 	public List<PayFormVo> list() {
 		return dao.list(sst);
 	}
 	
-	//결재대기 목록 조회
 	public List<PayFormVo> ingApprove() {
 		return dao.ingApprove(sst);
 	}
 	
-	//결재완료 목록 조회
 	public List<PayFormVo> edApproved() {
 		return dao.edApprove(sst);
 	}
 	
-	//지출결의서 작성
 	public int write(PayFormVo vo) {
 		return dao.write(sst, vo);
 	}
 	
-	//지출결의서 승인
 	public int apply(PayFormVo vo) {
 		return dao.apply(sst, vo);
 	}
 	
-	//지출결의서 반려
 	public int rejection(PayFormVo vo) {
 		return dao.rejection(sst, vo);
 	}
 	
-	//지출결의서 삭제
 	public int delete(String no) {
 		return dao.delete(sst, no);
 	}
