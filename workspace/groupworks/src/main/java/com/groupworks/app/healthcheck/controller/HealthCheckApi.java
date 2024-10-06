aa13ceda0d12414baac168e07845d3435aca68fe
jiseop <jiseop992@gmail.com>
2024-10-06 21:20:13 +0900

    도커 설정 및 불필요 파일 삭제

--- workspace/groupworks/src/main/java/com/groupworks/app/healthcheck/controller/HealthCheckApi.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/healthcheck/controller/HealthCheckApi.java
@@ -2,6 +2,7 @@ package com.groupworks.app.healthcheck.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class HealthCheckApi {
 
+    @Value("${DB_URL}")
+    private String dbUrl;
+
+    @Value("${DB_USER_NAME}")
+    private String dbUserName;
+
+    @Value("${DB_DRIVER_CLASS_NAME}")
+    private String dbDriverClassName;
+
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Success Health Check");
     }
+
+
 }
